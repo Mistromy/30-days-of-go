545e90478db56953c8faa9d3486c48cbb29cc220
Mist <125371422+Mistromy@users.noreply.github.com>
2026-02-01 15:48:35 +0000

    added deleting, fixed a few issues.

--- day2/main.go
+++ day2/main.go
@@ -34,19 +34,35 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q":
 			return m, tea.Quit
-
-		case "up":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down":
-			if m.cursor < len(m.items)-1 {
-				m.cursor++
-			}
-
-		case "enter":
-			m.items[m.cursor].enabled = !m.items[m.cursor].enabled
 		}
+		if len(m.items) >= 1 {
+			switch msg.String() {
+
+			case "up":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+			case "down":
+				if m.cursor < len(m.items)-1 {
+					m.cursor++
+				}
+
+			case "enter":
+				m.items[m.cursor].enabled = !m.items[m.cursor].enabled
+
+			case "backspace":
+				if m.cursor != len(m.items) {
+					m.items = append(m.items[:m.cursor], m.items[m.cursor+1:]...)
+					if m.cursor >= len(m.items) {
+						m.cursor = len(m.items) - 1
+					}
+				}
+			}
+		}
+	}
+	if len(m.items) == 0 {
+		m.instructions = "No items left!"
+		return m, nil
 	}
 	m.instructions = m.items[m.cursor].name
 	return m, nil
